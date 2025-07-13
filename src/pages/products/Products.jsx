84eba773a8775f0563bf2f96cabcae3c57a381c9
Bongani <bbongs421@gmail.com>
2025-07-13 22:39:33 +0200

    make product tabs sticky

--- src/pages/products/Products.jsx
+++ src/pages/products/Products.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { airFilters, labEquipments } from "/src/data";
 import ProductCard from "../../components/ProductCard";
+import { HashLink } from "react-router-hash-link";
 
 const Products = () => {
   const [productTab, setProductTab] = useState("filters");
@@ -11,100 +12,106 @@ const Products = () => {
 
   return (
     <div className="pt-17">
-      {/* PRODUCT TABS */}
-      <div className="bg-zinc-100 rounded-lg place-self-center space-x-5 px-4 py-2 my-10">
-        <span
-          className={`${
-            productTab === "filters" &&
-            "font-semibold border-b-2 border-primary"
-          } pb-1 hover:cursor-pointer`}
-          onClick={() => setProductTab("filters")}
-        >
-          Filters
-        </span>
-        <span
-          className={`${
-            productTab === "labEquipments" &&
-            "font-semibold border-b-2 border-primary"
-          } pb-1 hover:cursor-pointer`}
-          onClick={() => setProductTab("labEquipments")}
-        >
-          Lab Equipments
-        </span>
+      <div id="products" className="relative">
+        {/* PRODUCT TABS */}
+        <div className="sticky top-18 bg-zinc-100 rounded-lg place-self-center space-x-5 px-4 py-2 my-10">
+          <HashLink smooth to="#products">
+            <span
+              className={`${
+                productTab === "filters" &&
+                "font-semibold border-b-2 border-primary"
+              } pb-1 hover:cursor-pointer`}
+              onClick={() => setProductTab("filters")}
+            >
+              Filters
+            </span>
+          </HashLink>
+          <HashLink smooth to="#products">
+            <span
+              className={`${
+                productTab === "labEquipments" &&
+                "font-semibold border-b-2 border-primary"
+              } pb-1 hover:cursor-pointer`}
+              onClick={() => setProductTab("labEquipments")}
+            >
+              Lab Equipments
+            </span>
+          </HashLink>
+        </div>
+
+        {/* FILTERS */}
+        {productTab === "filters" && (
+          <div className="mx-5 sm:mx-20">
+            {/* PRIMARY AIR FILTERS */}
+            <div className="mb-20">
+              <h2 className="text-xl sm:text-2xl font-semibold mb-5">
+                Primary Air Filters
+              </h2>
+              <div className="grid grid-cols-2 md:grid-cols-3 gap-3 sm:gap-10">
+                {airFilters["primary"]?.map((airFilter) => (
+                  <ProductCard
+                    key={airFilter.id}
+                    image={airFilter.image}
+                    title={airFilter.title}
+                    file={`/files/airFilters/primary/` + airFilter.pdf}
+                  />
+                ))}
+              </div>
+            </div>
+
+            {/* SECONDARY AIR FILTERS */}
+            <div className="mb-20">
+              <h2 className="text-xl sm:text-2xl font-semibold mb-5">
+                Secondary Air Filters
+              </h2>
+              <div className="grid grid-cols-2 md:grid-cols-3 gap-3 sm:gap-10">
+                {airFilters["secondary"]?.map((airFilter) => (
+                  <ProductCard
+                    key={airFilter.id}
+                    image={airFilter.image}
+                    title={airFilter.title}
+                    file={`/files/airFilters/secondary/` + airFilter.pdf}
+                  />
+                ))}
+              </div>
+            </div>
+
+            {/* TERTIARY AIR FILTERS */}
+            <div className="mb-20">
+              <h2 className="text-xl sm:text-2xl font-semibold mb-5">
+                Tertiary Air Filters
+              </h2>
+              <div className="grid grid-cols-2 md:grid-cols-3 gap-3 sm:gap-10">
+                {airFilters["tertiary"]?.map((airFilter) => (
+                  <ProductCard
+                    key={airFilter.id}
+                    image={airFilter.image}
+                    title={airFilter.title}
+                    file={`/files/airFilters/tertiary/` + airFilter.pdf}
+                  />
+                ))}
+              </div>
+            </div>
+          </div>
+        )}
+
+        {/* LAB EQUIPMENT */}
+        {productTab === "labEquipments" && (
+          <div className="mb-20 mx-5 sm:mx-20">
+            <h2 className="text-2xl font-semibold mb-5">Lab Equipments</h2>
+            <div className="grid grid-cols-2 md:grid-cols-3 gap-3 sm:gap-10">
+              {labEquipments?.map((equipment) => (
+                <ProductCard
+                  key={equipment.id}
+                  image={equipment.image}
+                  title={equipment.title}
+                  file={`/files/labEquipment/` + equipment.pdf}
+                />
+              ))}
+            </div>
+          </div>
+        )}
       </div>
-
-      {/* FILTERS */}
-      {productTab === "filters" && (
-        <div className="mx-5 sm:mx-20">
-          {/* PRIMARY AIR FILTERS */}
-          <div className="mb-20">
-            <h2 className="text-xl sm:text-2xl font-semibold mb-5">
-              Primary Air Filters
-            </h2>
-            <div className="grid grid-cols-2 md:grid-cols-3 gap-3 sm:gap-10">
-              {airFilters["primary"]?.map((airFilter) => (
-                <ProductCard
-                  key={airFilter.id}
-                  image={airFilter.image}
-                  title={airFilter.title}
-                  file={`/files/airFilters/primary/` + airFilter.pdf}
-                />
-              ))}
-            </div>
-          </div>
-
-          {/* SECONDARY AIR FILTERS */}
-          <div className="mb-20">
-            <h2 className="text-xl sm:text-2xl font-semibold mb-5">
-              Secondary Air Filters
-            </h2>
-            <div className="grid grid-cols-2 md:grid-cols-3 gap-3 sm:gap-10">
-              {airFilters["secondary"]?.map((airFilter) => (
-                <ProductCard
-                  key={airFilter.id}
-                  image={airFilter.image}
-                  title={airFilter.title}
-                  file={`/files/airFilters/secondary/` + airFilter.pdf}
-                />
-              ))}
-            </div>
-          </div>
-
-          {/* TERTIARY AIR FILTERS */}
-          <div className="mb-20">
-            <h2 className="text-xl sm:text-2xl font-semibold mb-5">
-              Tertiary Air Filters
-            </h2>
-            <div className="grid grid-cols-2 md:grid-cols-3 gap-3 sm:gap-10">
-              {airFilters["tertiary"]?.map((airFilter) => (
-                <ProductCard
-                  key={airFilter.id}
-                  image={airFilter.image}
-                  title={airFilter.title}
-                  file={`/files/airFilters/tertiary/` + airFilter.pdf}
-                />
-              ))}
-            </div>
-          </div>
-        </div>
-      )}
-
-      {/* LAB EQUIPMENT */}
-      {productTab === "labEquipments" && (
-        <div className="mb-20 mx-5 sm:mx-20">
-          <h2 className="text-2xl font-semibold mb-5">Lab Equipments</h2>
-          <div className="grid grid-cols-2 md:grid-cols-3 gap-3 sm:gap-10">
-            {labEquipments?.map((equipment) => (
-              <ProductCard
-                key={equipment.id}
-                image={equipment.image}
-                title={equipment.title}
-                file={`/files/labEquipment/` + equipment.pdf}
-              />
-            ))}
-          </div>
-        </div>
-      )}
 
       {/* CONTACT US */}
       <div className="grid sm:grid-cols-2 gap-10 mx-5 sm:mx-20 my-20">
