5afabf0dce0bfd16345990300c48436459292171
Bongani <bbongs421@gmail.com>
2025-07-20 09:47:51 +0200

    update all pruducts button

--- src/components/ExploreProductsButton.jsx
+++ src/components/ExploreProductsButton.jsx
@@ -6,7 +6,7 @@ const ExploreProductsButton = () => {
     <Link to="/products">
       <button
         type="button"
-        className="bg-primary/90 text-white font-medium rounded-lg hover:bg-transparent hover:text-primary border border-primary hover:cursor-pointer py-2 px-4"
+        className="from-primary to-primary/60 bg-gradient-to-r text-white font-medium rounded-lg hover:from-transparent hover:text-primary border border-primary hover:cursor-pointer py-2 px-4"
       >
         Explore All Products
       </button>
